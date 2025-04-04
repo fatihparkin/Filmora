eaa0776e798305d21caebd74d81a27e777a67300
FatihParkın <61151029+fatihparkin@users.noreply.github.com>
2025-04-04 15:29:42 +0300

    Yorumlar endpoint’i eklendi.

--- app/src/main/java/com/fatihparkin/filmora/data/remote/ApiService.kt
+++ app/src/main/java/com/fatihparkin/filmora/data/remote/ApiService.kt
@@ -1,10 +1,10 @@
 package com.fatihparkin.filmora.data.remote
 
 import com.fatihparkin.filmora.BuildConfig
-import com.fatihparkin.filmora.data.model.MovieResponse
-import com.fatihparkin.filmora.data.model.GenreResponse
+import com.fatihparkin.filmora.data.model.*
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface ApiService {
@@ -27,4 +27,30 @@ interface ApiService {
         @Query("with_genres") genreId: Int,
         @Query("page") page: Int = 1
     ): Response<MovieResponse>
+
+    @GET("movie/{movie_id}")
+    suspend fun getMovieDetail(
+        @Path("movie_id") movieId: Int,
+        @Query("api_key") apiKey: String = BuildConfig.TMDB_API_KEY
+    ): Response<Movie>
+
+    @GET("movie/{movie_id}/videos")
+    suspend fun getMovieVideos(
+        @Path("movie_id") movieId: Int,
+        @Query("api_key") apiKey: String = BuildConfig.TMDB_API_KEY
+    ): Response<VideoResponse>
+
+    @GET("movie/{movie_id}/credits")
+    suspend fun getMovieCredits(
+        @Path("movie_id") movieId: Int,
+        @Query("api_key") apiKey: String = BuildConfig.TMDB_API_KEY
+    ): Response<CastResponse>
+
+    @GET("movie/{movie_id}/reviews")
+    suspend fun getMovieReviews(
+        @Path("movie_id") movieId: Int,
+        @Query("api_key") apiKey: String = BuildConfig.TMDB_API_KEY,
+        @Query("page") page: Int = 1
+    ): Response<ReviewResponse>
+
 }
