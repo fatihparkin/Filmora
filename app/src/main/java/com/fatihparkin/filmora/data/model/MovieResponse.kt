704d57bd1fca0f27777e3acf4837bf0300a9d192
FatihParkın <61151029+fatihparkin@users.noreply.github.com>
2025-04-28 16:03:44 +0300

    Movie classının serializable yapılması

--- app/src/main/java/com/fatihparkin/filmora/data/model/MovieResponse.kt
+++ app/src/main/java/com/fatihparkin/filmora/data/model/MovieResponse.kt
@@ -1,5 +1,5 @@
 package com.fatihparkin.filmora.data.model
-
+import java.io.Serializable
 data class MovieResponse(
     val page: Int,
     val results: List<Movie>,
@@ -7,12 +7,14 @@ data class MovieResponse(
     val total_results: Int
 )
 
+
+
 data class Movie(
-    val id: Int,
-    val title: String,
-    val poster_path: String,
-    val overview: String,
-    val release_date: String,
-    val backdrop_path: String,
-    val vote_average: Double
-)
+    val id: Int = 0,
+    val title: String = "",
+    val poster_path: String = "",
+    val overview: String = "",
+    val release_date: String = "",
+    val backdrop_path: String = "",
+    val vote_average: Double = 0.0
+) : Serializable
